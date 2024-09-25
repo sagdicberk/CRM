16ae0290a1f16e0949685266dc2aaca5330b67f5
sagdicberk <sagdicberk@gmail.com>
2024-09-25 16:46:48 +0300

    I created EndPoint for authentication and I tried the methods with postman. this class working correctly.

---
+++ src/main/java/com/sgdcbrk/crm/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.sgdcbrk.crm.controller;
+
+import com.sgdcbrk.crm.auth.AuthService;
+import com.sgdcbrk.crm.dto.requests.LoginRequest;
+import com.sgdcbrk.crm.dto.requests.RegisterRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+}
