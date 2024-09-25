2e6563dc470146a637cd04aeef54f82274d6091d
sagdicberk <sagdicberk@gmail.com>
2024-09-25 14:49:12 +0300

    Created for Email and password authentication.

---
+++ src/main/java/com/sgdcbrk/crm/auth/UserDetailsServiceImp.java
@@ -0,0 +1,26 @@
+package com.sgdcbrk.crm.auth;
+
+import com.sgdcbrk.crm.model.User;
+import com.sgdcbrk.crm.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImp implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+        return new UserDetailsImp(user);
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        throw new RuntimeException("Giriş işlemleri email ile yapılacaktır. Bu fonksiyon kullanılmıyor.");
+    }
+}
