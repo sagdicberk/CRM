b3d50d6b0bafe976ec81afa98f40ef23f031f7d2
sagdicberk <sagdicberk@gmail.com>
2024-09-25 14:17:42 +0300

    Update and create functions are updated.

--- src/main/java/com/sgdcbrk/crm/business/concretes/UserManager.java
+++ src/main/java/com/sgdcbrk/crm/business/concretes/UserManager.java
@@ -4,6 +4,7 @@ import com.sgdcbrk.crm.business.abstracts.UserService;
 import com.sgdcbrk.crm.model.User;
 import com.sgdcbrk.crm.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,16 +13,20 @@ import java.util.List;
 @AllArgsConstructor
 public class UserManager implements UserService {
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
     @Override
     public void updateUser(long id, User user) {
         User existingUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User with id " + id + " not found"));
+        existingUser.setUsername(user.getUsername());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(existingUser);
     }
 
