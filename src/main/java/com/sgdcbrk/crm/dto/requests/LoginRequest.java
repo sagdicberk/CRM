8204958374bdbd41c3a513fced5b27a1aee889b2
sagdicberk <sagdicberk@gmail.com>
2024-09-25 16:07:02 +0300

    these 2 dto added. request based dto used for this project. these two dto is for the authentication.

---
+++ src/main/java/com/sgdcbrk/crm/dto/requests/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.sgdcbrk.crm.dto.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
