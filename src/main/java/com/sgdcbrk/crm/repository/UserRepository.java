fd4cc120772bcdf1f0e58a1b6f1a91e7855ec5d8
sagdicberk <sagdicberk@gmail.com>
2024-09-25 14:50:09 +0300

    Email query added. this query created for authentication

--- src/main/java/com/sgdcbrk/crm/repository/UserRepository.java
+++ src/main/java/com/sgdcbrk/crm/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.sgdcbrk.crm.repository;
 import com.sgdcbrk.crm.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
