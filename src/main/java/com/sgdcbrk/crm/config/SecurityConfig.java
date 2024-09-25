6778096bd9ff7592d09e14239bdc33b45f68b1f1
sagdicberk <sagdicberk@gmail.com>
2024-09-25 14:50:34 +0300

    HttpBasic is deleted

--- src/main/java/com/sgdcbrk/crm/config/SecurityConfig.java
+++ src/main/java/com/sgdcbrk/crm/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +19,7 @@ public class SecurityConfig {
                 .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .anyRequest().permitAll()
-                ).httpBasic(HttpBasicConfigurer::disable);
+                );
 
         return http.build();
     }
