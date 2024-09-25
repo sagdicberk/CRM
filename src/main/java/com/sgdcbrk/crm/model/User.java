1e5f19f1b62f3a252f138ad86078b70a67b79807
sagdicberk <sagdicberk@gmail.com>
2024-09-25 17:24:04 +0300

    Column annotation is added for safe app

--- src/main/java/com/sgdcbrk/crm/model/User.java
+++ src/main/java/com/sgdcbrk/crm/model/User.java
@@ -19,8 +19,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String username;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
