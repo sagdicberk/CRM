9681b3a2013675cf1b4e7e5ae21af5297c86b90b
sagdicberk <sagdicberk@gmail.com>
2024-09-26 17:45:55 +0300

    UserDetails error fixed

--- src/main/java/com/sgdcbrk/crm/business/concretes/auth/AuthService.java
+++ src/main/java/com/sgdcbrk/crm/business/concretes/auth/AuthService.java
@@ -3,35 +3,39 @@ package com.sgdcbrk.crm.business.concretes.auth;
 import com.sgdcbrk.crm.business.abstracts.UserService;
 import com.sgdcbrk.crm.dto.requests.LoginRequest;
 import com.sgdcbrk.crm.dto.requests.RegisterRequest;
-import lombok.AllArgsConstructor;
+import com.sgdcbrk.crm.util.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     public String authenticate(LoginRequest request) {
-        try{
+        try {
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            return "Logged in as " + userDetails.getUsername();
-        }catch(AuthenticationException e){
+
+            UserDetailsImp userDetails = (UserDetailsImp) authentication.getPrincipal();
+
+            return jwtUtil.generateToken(userDetails.getEmail());
+
+        } catch (AuthenticationException e) {
             return "Invalid email or password " + e.getMessage();
         }
     }
 
     public String register(RegisterRequest request) {
-        try{
+        try {
             userService.createUser(request);
             return "User registered successfully";
-        }catch (Exception e){
+        } catch (Exception e) {
             return "Registry operating fall down. Try Again " + e.getMessage();
         }
 
