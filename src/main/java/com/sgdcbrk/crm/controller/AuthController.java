63b82b7e3cd185676377b02248da9f9923188776
sagdicberk <sagdicberk@gmail.com>
2024-09-29 00:37:50 +0300

    deleted unused imports

--- src/main/java/com/sgdcbrk/crm/controller/AuthController.java
+++ src/main/java/com/sgdcbrk/crm/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.sgdcbrk.crm.controller;
 
 import com.sgdcbrk.crm.business.concretes.auth.AuthService;
-import com.sgdcbrk.crm.dto.requests.LoginRequest;
-import com.sgdcbrk.crm.dto.requests.RegisterRequest;
+import com.sgdcbrk.crm.dto.user.requests.LoginRequest;
+import com.sgdcbrk.crm.dto.user.requests.RegisterRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
