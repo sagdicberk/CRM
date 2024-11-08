7b042371af57097ea8483acca1cba0cd015bca14
sagdicberk <sagdicberk@gmail.com>
2024-11-09 02:59:23 +0300

    UUID error is fixed. Changed to Identity

--- src/main/java/com/sgdcbrk/crm/model/customer/Customer.java
+++ src/main/java/com/sgdcbrk/crm/model/customer/Customer.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
